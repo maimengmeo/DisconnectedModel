a0bff62b8cce0948b8655151929c1e0a2024c3f3
maimengmeo <mai291294@gmail.com>
2023-03-28 16:51:36 -0400

    created 3 tables

--- A2TuyetMaiPham/Data.cs
+++ A2TuyetMaiPham/Data.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace A2TuyetMaiPham
 {
@@ -12,5 +14,47 @@ namespace A2TuyetMaiPham
                                          Initial Catalog=WorldDB;
                                          Integrated Security=True";
         public static string ConnectionString { get => connStr; }
+
+        public DataTable GetContinents()
+        {
+            string query = "Select ContinentId, ContinentName from Continent";
+
+            SqlConnection conn = new SqlConnection(Data.ConnectionString);
+
+            SqlDataAdapter adp = new SqlDataAdapter(query, conn);
+
+            DataSet ds = new DataSet();
+
+            adp.Fill(ds, "Continent");
+
+            DataTable tblContinents = ds.Tables["Continent"];
+
+            return tblContinents;
+
+        }
+
+        public DataTable GetCountry()
+        {
+            string query = "Select * from City";
+            SqlConnection conn = new SqlConnection(Data.ConnectionString);
+            SqlDataAdapter adp = new SqlDataAdapter(query, conn);
+            DataSet ds = new DataSet();
+            adp.Fill(ds, "Country");
+            DataTable tblCountry = ds.Tables["Country"];
+            return tblCountry;
+        }
+
+        public DataTable GetCity()
+        {
+            string query = "select * from City";
+            SqlConnection conn = new SqlConnection(Data.ConnectionString);
+            SqlDataAdapter adp = new SqlDataAdapter(query, conn);
+            DataSet ds = new DataSet();
+            adp.Fill(ds, "City");
+            DataTable tblCity = ds.Tables["City"];
+            return tblCity;
+        }
     }
+
+    
 }
