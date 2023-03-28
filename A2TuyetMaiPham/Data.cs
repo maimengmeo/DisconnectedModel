d7ea39cfd46b88530b938369b4e62556f44093c5
maimengmeo <mai291294@gmail.com>
2023-03-28 16:35:01 -0400

    first commit

---
+++ A2TuyetMaiPham/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2TuyetMaiPham
+{
+    public class Data
+    {
+        private static string connStr = @"Data Source=(LocalDB)\MSSQLLocalDB;
+                                         Initial Catalog=WorldDB;
+                                         Integrated Security=True";
+        public static string ConnectionString { get => connStr; }
+    }
+}
